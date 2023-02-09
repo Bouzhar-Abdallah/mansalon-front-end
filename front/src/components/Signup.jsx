1e54390997753a67f1f1fa8670825fbda37c6c86
abdallah <abddoullah.bouzhar@gmail.com>
2023-02-09 11:10:07 +0100

    usestate in form

--- front/src/components/Signup.jsx
+++ front/src/components/Signup.jsx
@@ -1,147 +1,169 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import {useState} from 'react'
 function Signup() {
+
+  const [email, setEmail] = useState('')
+  const [prenom, setprenom] = useState('')
+  const [nom, setnom] = useState('')
+  const [numero_tel, setNumerotel] = useState('')
+  const utilisateur = {
+    nom: nom,
+    prenom: prenom,
+    enail: email,
+    numero_tel: numero_tel
+  }
   return (
-    <section class="py-10 bg-gray-100  bg-opacity-50 h-screen">
-      <div class="mx-auto container max-w-2xl md:w-3/4 shadow-md">
-        <div class="bg-gray-100 p-4 border-t-2 bg-opacity-5 border-red-500 rounded-t">
-          <div class="max-w-sm mx-auto md:w-full md:mx-0">
-            <div class="inline-flex items-center space-x-4">
-              <h1 class="text-gray-600">Creaction nouveau compte</h1>
+    <section className="py-10 bg-gray-100  bg-opacity-50 h-screen">
+      <div className="mx-auto container max-w-2xl md:w-3/4 shadow-md">
+        <div className="bg-gray-100 p-4 border-t-2 bg-opacity-5 border-red-500 rounded-t">
+          <div className="max-w-sm mx-auto md:w-full md:mx-0">
+            <div className="inline-flex items-center space-x-4">
+              <h1 className="text-gray-600">Creaction nouveau compte</h1>
             </div>
           </div>
         </div>
-        <div class="bg-white space-y-6">
-          <div class="md:inline-flex space-y-4 md:space-y-0 w-full p-4 text-gray-500 items-center">
-            <h2 class="md:w-1/3 max-w-sm mx-auto">Account</h2>
-            <div class="md:w-2/3 max-w-sm mx-auto">
-              <label class="text-sm text-gray-400">Email</label>
-              <div class="w-full inline-flex border">
-                <div class="pt-2 w-1/12 bg-gray-100 bg-opacity-50">
+        <div className="bg-white space-y-6">
+          <div className="md:inline-flex space-y-4 md:space-y-0 w-full p-4 text-gray-500 items-center">
+            <h2 className="md:w-1/3 max-w-sm mx-auto">Account</h2>
+            <div className="md:w-2/3 max-w-sm mx-auto">
+              <label className="text-sm text-gray-400">Email</label>
+              <div className="w-full inline-flex border">
+                <div className="pt-2 w-1/12 bg-gray-100 bg-opacity-50">
                   <svg
                     fill="none"
-                    class="w-6 text-gray-400 mx-auto"
+                    className="w-6 text-gray-400 mx-auto"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                     />
                   </svg>
                 </div>
                 <input
                   type="email"
-                  class="w-11/12 focus:outline-none focus:text-gray-600 p-2"
+                  className="w-11/12 focus:outline-none focus:text-gray-600 p-2"
                   placeholder="email@example.com"
-                  disabled
-                />
+                  onChange={ Event => setEmail(Event.target.value) }
+                  />
               </div>
+                  <p>{ email }</p>
             </div>
           </div>
 
           <hr />
-          <div class="md:inline-flex  space-y-4 md:space-y-0  w-full p-4 text-gray-500 items-center">
-            <h2 class="md:w-1/3 mx-auto max-w-sm">Personal info</h2>
-            <div class="md:w-2/3 mx-auto max-w-sm space-y-5">
+          <div className="md:inline-flex  space-y-4 md:space-y-0  w-full p-4 text-gray-500 items-center">
+            <h2 className="md:w-1/3 mx-auto max-w-sm">Personal info</h2>
+            <div className="md:w-2/3 mx-auto max-w-sm space-y-5">
               <div>
-                <label class="text-sm text-gray-400">first name</label>
-                <div class="w-full inline-flex border">
-                  <div class="w-1/12 pt-2 bg-gray-100">
+                <label className="text-sm text-gray-400">first name</label>
+                <div className="w-full inline-flex border">
+                  <div className="w-1/12 pt-2 bg-gray-100">
                     <svg
                       fill="none"
-                      class="w-6 text-gray-400 mx-auto"
+                      className="w-6 text-gray-400 mx-auto"
                       viewBox="0 0 24 24"
                       stroke="currentColor"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
                       />
                     </svg>
                   </div>
                   <input
                     type="text"
-                    class="w-11/12 focus:outline-none focus:text-gray-600 p-2"
+                    className="w-11/12 focus:outline-none focus:text-gray-600 p-2"
                     placeholder="Charly Olivas"
+                    onChange={ Event => setprenom(Event.target.value)}
                   />
                 </div>
+                <p>{prenom}</p>
               </div>
               <div>
-                <label class="text-sm text-gray-400">last name</label>
-                <div class="w-full inline-flex border">
-                  <div class="w-1/12 pt-2 bg-gray-100">
+                <label className="text-sm text-gray-400">last name</label>
+                <div className="w-full inline-flex border">
+                  <div className="w-1/12 pt-2 bg-gray-100">
                     <svg
                       fill="none"
-                      class="w-6 text-gray-400 mx-auto"
+                      className="w-6 text-gray-400 mx-auto"
                       viewBox="0 0 24 24"
                       stroke="currentColor"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
                       />
                     </svg>
                   </div>
                   <input
                     type="text"
-                    class="w-11/12 focus:outline-none focus:text-gray-600 p-2"
-                    placeholder="Charly Olivas"
+                    className="w-11/12 focus:outline-none focus:text-gray-600 p-2"
+                    placeholder="Nom"
+                    onChange={Event => setnom(Event.target.value)}
                   />
                 </div>
+                <p>{nom}</p>
               </div>
               <div>
-                <label class="text-sm text-gray-400">Phone number</label>
-                <div class="w-full inline-flex border">
-                  <div class="pt-2 w-1/12 bg-gray-100">
+                <label className="text-sm text-gray-400">Phone number</label>
+                <div className="w-full inline-flex border">
+                  <div className="pt-2 w-1/12 bg-gray-100">
                     <svg
                       fill="none"
-                      class="w-6 text-gray-400 mx-auto"
+                      className="w-6 text-gray-400 mx-auto"
                       viewBox="0 0 24 24"
                       stroke="currentColor"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"
                       />
                     </svg>
                   </div>
                   <input
                     type="text"
-                    class="w-11/12 focus:outline-none focus:text-gray-600 p-2"
+                    className="w-11/12 focus:outline-none focus:text-gray-600 p-2"
                     placeholder="12341234"
+                    onChange={Event => setNumerotel(Event.target.value)}
                   />
                 </div>
+                <p>
+                  {numero_tel}
+                </p>
               </div>
             </div>
           </div>
 
           <hr />
-          <div class="md:inline-flex w-full space-y-4 md:space-y-0 p-8 text-gray-500 items-center">
-            <div class="md:w-3/12 text-center md:pl-6 mx-auto">
-              <button class="text-white w-full mx-auto max-w-sm rounded text-center bg-red-600 py-2 px-4 inline-flex items-center focus:outline-none md:float-right">
+          <div className="md:inline-flex w-full space-y-4 md:space-y-0 p-8 text-gray-500 items-center">
+            <div className="md:w-3/12 text-center md:pl-6 mx-auto">
+              <button onClick={()=>{
+                console.log(utilisateur)
+              }} className="text-white w-full mx-auto max-w-sm rounded text-center bg-red-600 py-2 px-4 inline-flex items-center focus:outline-none md:float-right">
                
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   className="w-4 text-white mr-2"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                   />
                 </svg>
