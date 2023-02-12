92f8518fff39996c37cb540f52a9ebcb2b268149
abdallah <abddoullah.bouzhar@gmail.com>
2023-02-12 15:48:10 +0100

    let's make protected routs

--- front/src/components/Signin.jsx
+++ front/src/components/Signin.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Component } from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import { auth } from "../utilities/auth";
 import axios from "axios";
 export default function Signin() {
 
