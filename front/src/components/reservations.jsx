73bbee037f2720f4ba80fd7988ef2fadcd57821f
abdallah <abddoullah.bouzhar@gmail.com>
2023-02-14 00:26:27 +0100

    works fine

--- front/src/components/reservations.jsx
+++ front/src/components/reservations.jsx
@@ -1,32 +1,97 @@
-import { Accordion } from "flowbite-react";
+import { Accordion, Button } from "flowbite-react";
 
 export default function reservations() {
   return (
-    <div>
-      <Accordion collapseAll={true}>
-        <Accordion.Panel>
-          <Accordion.Title>What is Flowbite?</Accordion.Title>
-          <Accordion.Content>
-            <p className="mb-2 text-gray-500 dark:text-gray-400">
-              Flowbite is an open-source library of interactive components built
-              on top of Tailwind CSS including buttons, dropdowns, modals,
-              navbars, and more.
-            </p>
-            <p className="text-gray-500 dark:text-gray-400">
-              Check out this guide to learn how to{" "}
-              <a
-                href="https://flowbite.com/docs/getting-started/introduction/"
-                className="text-blue-600 hover:underline dark:text-blue-500"
-              >
-                get started
-              </a>{" "}
-              and start developing websites even faster with components on top
-              of Tailwind CSS.
-            </p>
-          </Accordion.Content>
-        </Accordion.Panel>
-        
-      </Accordion>
+    <div className="p-5">
+      <div className="lg:w-2/3 mx-auto">
+        <Accordion collapseAll={true}>
+          <Accordion.Panel>
+            <Accordion.Title>What is Flowbite?</Accordion.Title>
+            <Accordion.Content>
+              <Button.Group>
+                <Button color="gray">8</Button>
+                <Button color="gray">9</Button>
+                <Button color="gray">10</Button>
+                <Button color="gray">11</Button>
+
+                <Button color="gray">14</Button>
+                <Button color="gray">15</Button>
+                <Button color="gray">16</Button>
+                <Button color="gray">17</Button>
+              </Button.Group>
+            </Accordion.Content>
+          </Accordion.Panel>
+          <Accordion.Panel>
+            <Accordion.Title>What is Flowbite?</Accordion.Title>
+            <Accordion.Content>
+              <div className="flex gap-10 justify-center">
+                <div className="flex flex-col">
+                  <div className="flex justify-center items-center mb-2">
+                    <h1 className="text-center">matain</h1>
+                  </div>
+                  <div className="flex flex-wrap gap-2 justify-center">
+                    <div>
+                      <Button className="w-12" color="light">
+                        8
+                      </Button>
+                    </div>
+                    <div>
+                      <Button className="w-12" color="light">
+                        9
+                      </Button>
+                    </div>
+                    <div>
+                      <Button className="w-12" color="failure">
+                        10
+                      </Button>
+                    </div>
+                    <div>
+                      <Button className="w-12" color="light">
+                        11
+                      </Button>
+                    </div>
+                  </div>
+                  {/* <div>
+                    <Button className="w-12" color="success"></Button>
+                  </div> */}
+                </div>
+                <div className="flex flex-col">
+                <div className="flex justify-center items-center mb-2">
+                    <h1 className="text-center">soir</h1>
+                  </div>
+                  <div className="flex flex-wrap gap-2 justify-center">
+                    <div>
+                      <Button className="w-12" color="light">
+                        8
+                      </Button>
+                    </div>
+                    <div>
+                      <Button className="w-12" color="light">
+                        9
+                      </Button>
+                    </div>
+                    <div>
+                      <Button className="w-12" color="failure">
+                        10
+                      </Button>
+                    </div>
+                    <div>
+                      <Button className="w-12" color="light">
+                        11
+                      </Button>
+                    </div>
+                  </div>
+                  {/* <div>
+                    <Button className="w-12" color="success"></Button>
+                  </div> */}
+                </div>
+                
+                
+              </div>
+            </Accordion.Content>
+          </Accordion.Panel>
+        </Accordion>
+      </div>
     </div>
   );
 }
