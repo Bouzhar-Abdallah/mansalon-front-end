7bf7a5c3eb035a03a6e638806421eb954751c33e
abdallah <abddoullah.bouzhar@gmail.com>
2023-02-14 15:26:40 +0100

    show date

--- front/src/components/day.jsx
+++ front/src/components/day.jsx
@@ -1,10 +1,28 @@
 import { Accordion, Button, Breadcrumb } from "flowbite-react";
 
-export default function Day() {
+export default function Day(props) {
+  
+
+
+  
+ 
+    //let dayname = weekday[props.day.getDay()];
+    console.log(props.day)
+  
   return (
-    
     <>
-          <div className="flex gap-10 justify-center">
+      <details className="p-4 rounded-lg bg-white">
+        <summary className="font-semibold" >
+          <div className="w-48 flex ">
+          <span className="ml-4 ">{props.day.dayName} </span>
+          <span className="ml-auto">{props.day.day} </span>
+          <span className="ml-4">{props.day.month} </span>
+          </div>
+
+        </summary>
+        <div className="mt-3">
+          <div className="text-sm leading-6 "></div>
+          <div className="flex gap-10 justify-center bg-white py-4">
             <div className="flex flex-col">
               <div className="flex justify-center items-center mb-2">
                 <h1 className="text-center">matain</h1>
@@ -66,7 +84,8 @@ export default function Day() {
                   </div> */}
             </div>
           </div>
-       </>
-    
+        </div>
+      </details>
+    </>
   );
 }
--- front/src/components/reservations.jsx
+++ front/src/components/reservations.jsx
@@ -3,28 +3,30 @@ import Day from "./day";
 
 export default function reservations() {
   const weekday = [
-    "Dimanche",
-    "Lundi",
-    "Mardi",
-    "Mercredi",
-    "Jeudi",
-    "Vendredi",
-    "Samedi",
-  ];
+  "Dimanche",
+  "Lundi",
+  "Mardi",
+  "Mercredi",
+  "Jeudi",
+  "Vendredi",
+  "Samedi",
+];
 
-  const d = new Date();
-  var days = [];
 
+  var days2 = [];
   for (var i = 0; i < 30; i++) {
     var date = new Date();
     date.setDate(date.getDate() + i);
-    days.push(date);
+    var jsonDate = { 
+      id:i,
+      year: date.getFullYear(), 
+      month: date.getMonth() + 1, 
+      day: date.getDate(),
+      dayName: weekday[date.getDay()]
+    };
+    days2.push(jsonDate);
   }
 
-  days.forEach((day) => {
-    let dayname = weekday[day.getDay()];
-    //console.log(dayname);
-  });
 
   return (
     <>
@@ -35,48 +37,14 @@ export default function reservations() {
 
       <div className="p-5 ">
         <div className="lg:w-2/3 mx-auto bg-gray-300 p-4 border-t-2 bg-opacity-20 border-red-500 rounded-t">
-         <h1 className="text-center my-4">hello</h1>
+          <h1 className="text-center my-4">hello</h1>
           <div className="w-full md:px-2 mx-auto space-y-2  p-2 border-t ">
-            <details className="p-4 rounded-lg bg-green-300">
-              <summary className="font-semibold">
-                How to create Accordion (FAQ) in react ?
-              </summary>
-              <div className="mt-3">
-                <div className="text-sm leading-6 ">
-                  <Day />
-                </div>
-              </div>
-            </details>
-            <details className="p-4 rounded-lg">
-              <summary className="font-semibold">
-                How to create Accordion (FAQ) in react ?
-              </summary>
-              <div className="mt-3">
-                <div className="text-sm leading-6 ">
-                  <Day />
-                </div>
-              </div>
-            </details>
-            <details className="p-4 rounded-lg">
-              <summary className="font-semibold">
-                How to create Accordion (FAQ) in react ?
-              </summary>
-              <div className="mt-3">
-                <div className="text-sm leading-6 ">
-                  <Day />
-                </div>
-              </div>
-            </details>
-            <details className="p-4 rounded-lg">
-              <summary className="font-semibold">
-                How to create Accordion (FAQ) in react ?
-              </summary>
-              <div className="mt-3">
-                <div className="text-sm leading-6 ">
-                  <Day />
-                </div>
-              </div>
-            </details>
+            
+            {days2.map((day)=>{
+              return <Day key={day.id} day={day} />
+            })}
+                  
+                
           </div>
         </div>
       </div>
