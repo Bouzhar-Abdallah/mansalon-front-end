2f2534e888a10252cc315edb9fea187c8895558a
abdallah <abddoullah.bouzhar@gmail.com>
2023-03-02 21:53:57 +0100

    reservation canceles

---
+++ front/src/components/confirmCancel.jsx
@@ -0,0 +1,105 @@
+import React, { useContext, useState } from "react";
+import { Button, Modal, Flowbite } from "flowbite-react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Test from "./test";
+import { UserContext } from "../utilities/UserContext";
+import { Navigate } from "react-router-dom";
+
+export default function ConfirmCancel(props) {
+  const { user } = useContext(UserContext);
+
+  let [reservation, setReservation] = useState();
+  let [visible, setVisible] = useState(true);
+  let constsetParentShow = props.setShow
+  console.log('#'+ visible +'#')
+  const notify = (string) => {
+    if (string == "success") {
+      toast.success("Reservation canceled", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    } else if (string == "failure") {
+      toast.error("something went wrong", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  };
+  //console.log(user.identifiant)
+  const cancelReservation = async () => {
+    const response = await axios.post(
+      "http://localhost:8888/api/home/cancelReservation",
+      { identifiant_utilisateur: user.identifiant },
+      { "Content-Type": "application/json" }
+    );
+
+    if (response.data.message == "deleted successefully") {
+      notify("success");
+    } else {
+      notify("failure");
+    }
+    
+    setVisible(false);
+    
+    window.location.reload(false);
+  };
+  return (
+    <>
+      <ToastContainer />
+
+      <React.Fragment>
+        <Modal
+          show={visible}
+          size="md"
+          popup={true}
+          onClose={() => {
+            setVisible(false);
+          }}
+        >
+          <Modal.Header />
+          <Modal.Body>
+            <div className="text-center">
+              <h3
+                className="mb-5 text-lg font-normal text-gray-500
+         dark:text-gray-400"
+              >
+                are you sure you wanna cancel this reservation ?
+                <div>
+                  <span className="">le {props.data.date_jour}</span>
+                  <span className=""> a {props.data.heure}:00</span>
+                </div>
+              </h3>
+              <div className="flex justify-center gap-4">
+                <Button color="success" onClick={cancelReservation}>
+                  yes cancel
+                </Button>
+                <Button
+                  color="failure"
+                  onClick={() => {
+                    setVisible(false);
+                  }}
+                >
+                  ignore
+                </Button>
+              </div>
+            </div>
+          </Modal.Body>
+        </Modal>
+      </React.Fragment>
+    </>
+  );
+}
--- front/src/components/notifications.jsx
+++ front/src/components/notifications.jsx
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Button, Modal } from "flowbite-react";
 import { useContext } from "react";
 import { UserContext } from "../utilities/UserContext";
+import ConfirmationModel from "./confirmationModel";
 import axios from "axios";
+import ConfirmCancel from "./confirmCancel";
 
 export default function Notifications() {
+  const [showConfirmation, setConfirmation] = useState(false);
   const [show, setShow] = useState(false);
   const [reservation, setReservation] = useState(null);
   const { user } = useContext(UserContext);
@@ -20,10 +23,14 @@ export default function Notifications() {
     );
     if (response.data.message == "one record found") {
       setReservation(response.data);
-      console.log(reservation);
+      
     }
   };
-
+  function confirmation() {
+       
+    setConfirmation(true)
+    
+ }
   useEffect(() => {
     fetchData();
   }, []);
@@ -56,66 +63,67 @@ export default function Notifications() {
           />
         </svg>
       </button>
+      
       <Modal show={show} onClose={onClose}>
         <Modal.Header>Votre prochain Rendez-vous: </Modal.Header>
         <Modal.Body>
           {reservation ? (
             <>
-            <div className="space-y-6">
-              <div className="overflow-hidden bg-white shadow sm:rounded-lg">
-                <div className="border-t border-gray-200">
-                  <dl>
-                    <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                      <dt className="text-sm font-medium text-gray-500">
-                        Full name
-                      </dt>
-                      <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                        {user.nom} {user.prenom}
-                      </dd>
-                    </div>
+            {showConfirmation ? <ConfirmCancel setShow={setConfirmation} show={showConfirmation} data={reservation.data} /> : <></>}
+              <div className="space-y-6">
+                <div className="overflow-hidden bg-white shadow sm:rounded-lg">
+                  <div className="border-t border-gray-200">
+                    <dl>
+                      <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">
+                          Full name
+                        </dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                          {user.nom} {user.prenom}
+                        </dd>
+                      </div>
 
-                    <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                      <dt className="text-sm font-medium text-gray-500">
-                        Date
-                      </dt>
-                      <dd className="mt-1  text-gray-900 sm:col-span-2 sm:mt-0">
-                        <span className="font-bold text-lg">
-                          {reservation.data.date_jour}
-                        </span>
-                        <span className="ml-2">à</span>
-                        <span className="mx-2 font-bold text-lg">
-                          {reservation.data.heure}
-                          :00
-                        </span>
-                        <span>heures</span>
-                      </dd>
-                    </div>
-                  </dl>
+                      <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">
+                          Date
+                        </dt>
+                        <dd className="mt-1  text-gray-900 sm:col-span-2 sm:mt-0">
+                          <span className="font-bold text-lg">
+                            {reservation.data.date_jour}
+                          </span>
+                          <span className="ml-2">à</span>
+                          <span className="mx-2 font-bold text-lg">
+                            {reservation.data.heure}
+                            :00
+                          </span>
+                          <span>heures</span>
+                        </dd>
+                      </div>
+                    </dl>
+                  </div>
                 </div>
               </div>
-            </div>
-             <Modal.Footer>
-             <button
-               onClick={() => setShow(false)}
-               type="button"
-               className="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
-             >
-               Edit
-             </button>
-             <button
-               onClick={() => setShow(false)}
-               type="button"
-               className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
-             >
-               Cancel
-             </button>
-           </Modal.Footer>
-           </>
+              <Modal.Footer>
+                <button
+                  onClick={() => setShow(false)}
+                  type="button"
+                  className="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={confirmation}
+                  type="button"
+                  className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+                >
+                  Cancel
+                </button>
+              </Modal.Footer>
+            </>
           ) : (
             <div>you have no reservation at the moment</div>
           )}
         </Modal.Body>
-       
       </Modal>
     </>
   );
